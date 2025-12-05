d6a8ef243d6c71fcaf7f97b7e3777ba97ad1cb77
Anthonyjlem <anthonyjlem@gmail.com>
2025-12-05 15:21:36 -0700

    Add VRAM profiler

--- profiler_utils.py
+++ profiler_utils.py
@@ -1,4 +1,27 @@
 import pstats
+import pynvml
+
+
+class VRAMProfiler:
+
+    def __init__(self):
+        pynvml.nvmlInit()
+        self._handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+        self._peak_vram = 9
+
+    def sample_vram(self):
+        mem = pynvml.nvmlDeviceGetMemoryInfo(self._handle)
+        used = mem.used
+        if used > self._peak_vram:
+            self._peak_vram = used
+        return used
+
+    def get_peak_vram(self):
+        return self._peak_vram
+
+    def shutdown(self):
+        pynvml.nvmlShutdown()
+
 
 if __name__ == "__main__":
     p = pstats.Stats('output.prof')

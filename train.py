5fc42a13f8ab45925abbbfbd7c8fa119701e21c2
Anthonyjlem <anthonyjlem@gmail.com>
2025-12-10 20:42:58 -0700

    Add functionality to save and load checkpoints
    - Add ReduceLROnPlateau scheduler
    - Save best training checkpoint during training
    - Add functionality to load model weights and optimizer state from
      checkpoints. Missing loading scheduler state from checkpoint

--- train.py
+++ train.py
@@ -26,11 +26,11 @@ VALIDATION_AND_EMA = False
 PROFILE_VRAM = False
 BATCH_SIZE = 8
 LR = 1e-4
-WARMUP_STEPS = 1000
+SCHEDULER = "reduce"  # warmup (linear warmup) or reduce (reduce learning rate on plateau)
+WARMUP_STEPS = 1000  # used if SCHEDULER is "warmup"
 EPOCHS = 10000
-TRAIN_MODEL_SAVE_PATH = "D:/External Drive/Anthony/sd3_checkpoints/model.pt"
-VAL_MODEL_SAVE_PATH = "D:/External Drive/Anthony/sd3_checkpoints/model_ema.pt"
-SAMPLE_IMG_PATH = "D:/External Drive/Anthony/sd3_checkpoints/sample_img.png"
+SAVE_DIR = "D:/External Drive/Anthony/sd3_checkpoints"
+CHECKPOINT = os.path.join(SAVE_DIR, "model.pt")
 
 
 @line_profiler.profile
@@ -69,7 +69,10 @@ def train_one_epoch(model, epoch, num_epochs, train_loader, vae, clip, optimizer
         if PROFILE_VRAM:
             vram_profiler.sample_vram()
         scaler.update()
-        scheduler.step()
+        if SCHEDULER == "warmup":
+            scheduler.step()
+        else:
+            scheduler.step(loss)
         loss = loss.detach().item()
         running_loss += loss
         if PROFILE_VRAM:
@@ -148,12 +151,14 @@ def train_loop(model, epochs, vae, clip, clip_tokenizer, train_loader, val_loade
     avg_train_loss = validate_one_epoch(model, 0, 1, train_loader, vae, clip, loss_fn)
     if VALIDATION_AND_EMA:
         best_val_loss = validate_one_epoch(model, 0, 1, val_loader, vae, clip, loss_fn)
+    else:
+        best_train_loss = avg_train_loss
     text = "A dog sitting on the inside of a white boat."  # ID: 400
     img = generate_sample(text, model, vae, clip, clip_tokenizer, num_steps=5)
-    Image.fromarray(img).save(SAMPLE_IMG_PATH)
+    Image.fromarray(img).save(os.path.join(SAVE_DIR, "sample_img_0.png"))
     log = {"Average Train Loss Per Epoch": avg_train_loss,
            "Epoch": 0,
-           "Smaple Image": wandb.Image(SAMPLE_IMG_PATH, caption=text)}
+           "Smaple Image": wandb.Image(os.path.join(SAVE_DIR, "sample_img_0.png"), caption=text)}
     if VALIDATION_AND_EMA:
         log["Average Validation Loss Per Epoch"] = best_val_loss
     wandb.log(log)
@@ -171,9 +176,9 @@ def train_loop(model, epochs, vae, clip, clip_tokenizer, train_loader, val_loade
             "scheduler_state_dict": scheduler.state_dict(),
             "avg_train_loss": avg_train_loss,
         }
-        tmp_save_file = os.path.join(os.path.dirname(TRAIN_MODEL_SAVE_PATH), "model_tmp.pt")
+        tmp_save_file = os.path.join(SAVE_DIR, "model_tmp.pt")
         torch.save(checkpoint, tmp_save_file)
-        os.replace(tmp_save_file, TRAIN_MODEL_SAVE_PATH)
+        os.replace(tmp_save_file, os.path.join(SAVE_DIR, "model.pt"))
         if VALIDATION_AND_EMA:
             model = model.to("cpu")
             ema_model = ema_model.to("cuda")
@@ -183,16 +188,16 @@ def train_loop(model, epochs, vae, clip, clip_tokenizer, train_loader, val_loade
                 "model_state_dict": ema_model.state_dict(),
                 "avg_val_loss": avg_val_loss,
             }
-            tmp_save_file = os.path.join(os.path.dirname(VAL_MODEL_SAVE_PATH), "model_ema_tmp.pt")
+            tmp_save_file = os.path.join(os.path.dirname(SAVE_DIR), "model_ema_tmp.pt")
             torch.save(checkpoint, tmp_save_file)
-            os.replace(tmp_save_file, VAL_MODEL_SAVE_PATH)
+            os.replace(tmp_save_file, os.path.join(SAVE_DIR, "model_ema.pt"))
             img = generate_sample(text, ema_model, vae, clip, clip_tokenizer, num_steps=5)
         else:
             img = generate_sample(text, model, vae, clip, clip_tokenizer, num_steps=5)
-        Image.fromarray(img).save(SAMPLE_IMG_PATH)
+        Image.fromarray(img).save(os.path.join(SAVE_DIR, "sample_img_{}.png".format(e+1)))
         log = {"Average Train Loss Per Epoch": avg_train_loss,
                "Epoch": e+1,
-               "Smaple Image": wandb.Image(SAMPLE_IMG_PATH, caption=text)}
+               "Smaple Image": wandb.Image(os.path.join(SAVE_DIR, "sample_img_{}.png".format(e+1)), caption=text)}
         if VALIDATION_AND_EMA:
             log["Average Validation Loss Per Epoch"] = avg_val_loss
         wandb.log(log)
@@ -200,10 +205,15 @@ def train_loop(model, epochs, vae, clip, clip_tokenizer, train_loader, val_loade
             if avg_val_loss < best_val_loss:
                 best_val_loss = avg_val_loss
                 state_dict = ema_model.state_dict()
-                tmp_save_file = os.path.join(os.path.dirname(VAL_MODEL_SAVE_PATH), "model_{}_tmp.safetensors".format(e+1))
+                tmp_save_file = os.path.join(SAVE_DIR, "model_{}_tmp.safetensors".format(e+1))
                 save_file(state_dict, tmp_save_file)
-                output_path = os.path.join(os.path.dirname(VAL_MODEL_SAVE_PATH), "model_{}.safetensors".format(e+1))
+                output_path = os.path.join(SAVE_DIR, "model_{}.safetensors".format(e+1))
                 os.replace(tmp_save_file, output_path)
+        else:
+            if avg_train_loss < best_train_loss:
+                best_train_loss = avg_train_loss
+                chkpt_save_file = os.path.join(SAVE_DIR, "model_{}.pt".format(e+1))
+                torch.save(checkpoint, chkpt_save_file)
 
 
 if __name__ == "__main__":
@@ -217,11 +227,20 @@ if __name__ == "__main__":
     clip.eval()
     vae.eval()
     model = DiT()
+    dit_chkpt = None
+    if CHECKPOINT:
+        dit_chkpt = torch.load(CHECKPOINT, map_location="cpu")
+        model.load_state_dict(dit_chkpt["model_state_dict"])
     if PROFILE_VRAM:
         print_model_size(model)
 
     train_loader, val_loader = build_dataloaders(BATCH_SIZE, clip_tokenizer)
     loss_fn = torch.nn.MSELoss()
     optimizer = bnb.optim.AdamW(model.parameters(), lr=LR)
-    scheduler = linear_warmup_scheduler(optimizer, WARMUP_STEPS)
+    if CHECKPOINT:
+        optimizer.load_state_dict(dit_chkpt["optimizer_state_dict"])
+    if SCHEDULER == "warmup":
+        scheduler = linear_warmup_scheduler(optimizer, WARMUP_STEPS)
+    else:
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=100)
     train_loop(model, EPOCHS, vae, clip, clip_tokenizer, train_loader, val_loader, optimizer, loss_fn, scheduler, LR, WARMUP_STEPS)
